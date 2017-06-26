d98a2bbc7a86d43666c1fc5a4f97ded706c357e2
adityathiru <T.Aditya@iiitb.org>
2017-06-26 11:39:15 +0530

    Updated country test file, Indented country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -11,18 +11,18 @@
 #
 
 class Country < ActiveRecord::Base
-include PublicActivity::Model
-tracked owner: Proc.new{ |controller, model| controller && controller.current_user }
+	include PublicActivity::Model
+	tracked owner: Proc.new{ |controller, model| controller && controller.current_user }
 
-  belongs_to :organization
-  belongs_to :user
-  has_many :sites, dependent: :destroy
+	belongs_to :organization
+	belongs_to :user
+	has_many :sites, dependent: :destroy
 
-  validates_presence_of :name, :organization_id
-  validates_uniqueness_of :name, case_sensitive: false, scope: :organization_id
-  # add search feature
-    def self.search(search)
+	validates_presence_of :name, :organization_id
+	validates_uniqueness_of :name, case_sensitive: false, scope: :organization_id
+  
+	# add search feature
+	def self.search(search)
     where("name iLIKE ?", "%#{search}%")
   end
-
 end
