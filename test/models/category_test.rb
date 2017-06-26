5d1f26e7e7e5880784d5e62e5e6145ce344c40a0
adityathiru <T.Aditya@iiitb.org>
2017-06-26 11:58:53 +0530

    Updated category test file, with extra tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -20,6 +20,12 @@ class CategoryTest < ActiveSupport::TestCase
     assert_not category.save, "Saved the category without its name"
   end
 
+  test "should save category with its field name" do
+    category = Category.new
+    category.name = "test_category"
+    assert category.save, "Couldn't save the category with its name"
+  end
+
   test "check if category present in search" do
   	# POPULATING DATABASE FROM FACTORY WITH OVERRIDE, TO VERIFY SEARCH
   	category1 = create(:category, name: "test_category_1")
@@ -45,8 +51,12 @@ class CategoryTest < ActiveSupport::TestCase
     assert_not category2.save, "Category name already exists."
   end
 
+  context "associations" do
+    should have_many(:articles)
+  end
+
   context "validations" do
   	should validate_presence_of(:name)
-  	should validate_uniqueness_of(:name)
+  	should validate_uniqueness_of(:name).case_insensitive
   end
 end
