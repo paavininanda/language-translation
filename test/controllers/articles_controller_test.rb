3da62b2ad1e78187a844973b5660f416bee52767
adityathiru <T.Aditya@iiitb.org>
2017-07-06 14:20:03 +0530

    Updates articles_controller and corresponding tests
    * Refactors articles_controller
    * Adds tests for create, update and state
    * Corrects state error in /test/controllers/contributor/articles_controller_test.rb

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -28,7 +28,7 @@ class ArticlesControllerTest < ActionController::TestCase
     assert_template layout: "layouts/application"
   end
 
-  test "should create photo and go to its show page" do
+  test "should create article and go to its show page" do
     category = create(:category)
 
     assert_difference('Article.count') do
@@ -39,16 +39,109 @@ class ArticlesControllerTest < ActionController::TestCase
                phonetic: "Pihiya",
                language_id: @language.id,
                picture: Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'picture', 'logo.jpg')),
+               audios: [Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'audio', 'test_audio.wav')), Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'audio', 'test_audio.wav'))],
                state: "published"
            }
     end
 
     assert_redirected_to article_path(assigns(:article))
+    assert_equal "Article successfully created.", flash[:notice]
+    assert_equal "published", assigns(:article).state
 
+  end
+
+  test "should not create article without picture" do
+    category = create(:category)
+
+      post :create,
+           article: {
+               category_id: category.id,
+               english: 'Knife',
+               phonetic: "Pihiya",
+               language_id: @language.id,
+               audios: [Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'audio', 'test_audio.wav')), Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'audio', 'test_audio.wav'))],
+               state: "published"
+           }
+
+    assert_equal "Sorry, failed to create article due to errors.", flash[:error]
+    assert_template(:new)
+
+  end
+
+  test "should not create article without category_id" do
+
+      post :create,
+           article: {
+               english: 'Knife',
+               phonetic: "Pihiya",
+               language_id: @language.id,
+               picture: Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'picture', 'logo.jpg')),
+               audios: [Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'audio', 'test_audio.wav')), Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'audio', 'test_audio.wav'))],
+               state: "published"
+           }
+
+    assert_equal "Sorry, failed to create article due to errors.", flash[:error]
+    assert_template(:new)
+
+  end
+
+  test "should update article" do
+    category = create(:category)
+
+    article = Article.create!({
+        language_id: @language.id,
+        category_id: category.id,
+        english: "Foods",
+        phonetic: "Keema",
+        picture: Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'picture', 'logo.jpg'))
+    })
+
+    get :edit, {id: article.id}
+
+    put :update,
+           {
+               id: article.id,
+               article: {
+                 phonetic: "Unavu",
+                 state: "published"
+               }
+           }
+
+    assert_redirected_to article_path(assigns(:article))
+    assert_equal "Unavu", assigns(:article).phonetic
+    assert_equal "Article successfully updated.", flash[:notice]
     assert_equal "published", assigns(:article).state
   end
 
-  test "should delete photo" do
+  test "should not update article without category_id and picture" do
+    category = create(:category)
+
+    article = Article.create!({
+        language_id: @language.id,
+        category_id: category.id,
+        english: "Foods",
+        phonetic: "Keema",
+        picture: Rack::Test::UploadedFile.new(File.join(Rails.root, 'test', 'support', 'picture', 'logo.jpg'))
+    })
+
+    get :edit, {id: article.id}
+
+    put :update,
+           {
+               id: article.id,
+               article: {
+                 phonetic: "Unavu",
+                 category_id: nil,
+                 picture: nil,
+                 state: "published"
+               }
+           }
+
+    assert_equal "Sorry, failed to update article due to errors.", flash[:error]
+    assert_template(:edit)
+  end
+
+  test "should delete article" do
     category = create(:category)
 
     article = Article.create!({
@@ -82,6 +175,8 @@ class ArticlesControllerTest < ActionController::TestCase
 
     post :publish, id: @article
     assert_equal "published", assigns(:article).state
+    assert_equal "The article has been published.", flash[:notice]
+    assert_redirected_to article_path(assigns(:article))
   end
 
   test "article change state to unpublish" do
@@ -100,6 +195,7 @@ class ArticlesControllerTest < ActionController::TestCase
 
     post :unpublish, id: @article
     assert_equal "draft", assigns(:article).state
+    assert_equal "The article has been drafted.", flash[:notice]
   end
 
 end
