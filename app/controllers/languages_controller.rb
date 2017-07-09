4fe698361adb3b76ef5f7642fb4b0e8ef7279930
adityathiru <T.Aditya@iiitb.org>
2017-07-09 14:23:25 +0530

    Updates languages_controller_test
    * Add tests for language search and duplicate languages
    * Refactors existing controller code
    * Fixes indentation

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,14 +1,16 @@
 class LanguagesController < ApplicationController
+  before_action :set_language, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
+  respond_to :html
 
   def index
-   @languages = Language.all
-  if params[:search]
-    @languages = Language.search(params[:search]).order("created_at DESC")
-  else
-    @languages = Language.all.order('created_at DESC')
-  end
+    @languages = Language.all
 
+    if params[:search]
+      @languages = Language.search(params[:search]).order("created_at DESC")
+    else
+      @languages = Language.all.order('created_at DESC')
+    end
   end
 
   def show
@@ -22,37 +24,47 @@ class LanguagesController < ApplicationController
   end
 
   def edit
-    @language = Language.find(params[:id])
   end
 
   def create
     @language = Language.new(language_params)
 
-    if @language.save
-      redirect_to edit_language_path(@language)
-    else
-      render 'new'
+    respond_with(@language) do |format|
+      if @language.save
+        flash[:notice] = "Language successfully created."
+        format.html { redirect_to edit_language_path(@language) }
+      else
+        flash[:error] = "Sorry, failed to create language due to errors."
+        format.html { render 'new' }
+      end
     end
   end
 
   def update
-    @language = Language.find(params[:id])
-
-    if @language.update(language_params)
-      redirect_to edit_language_path(@language)
-    else
-      render 'edit'
+    respond_with(@language) do |format|
+      if @language.update(language_params)
+        flash[:notice] = "Language successfully updated."
+        format.html { redirect_to edit_language_path(@language) }
+      else
+        flash[:error] = "Sorry, failed to update language due to errors."
+        format.html { render 'edit' }
+      end
     end
   end
 
   def destroy
-    @language = Language.find(params[:id])
     @language.destroy
 
+    flash[:notice] = "Language has been deleted."
     redirect_to languages_path
   end
 
   private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_language
+    @language = Language.find(params[:id])
+  end
+
   # Never trust parameters from the scary internet, only allow the white list through.
   def language_params
     params.require(:language).permit(:name)
